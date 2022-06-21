108e008fefc928dadbab6ef114a345375277748f
wensydesousa <wensys013@gmail.com>
2022-06-21 07:24:02 -0700

    updates

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
-const $cellElems = $('[data-cell]')
+//variables for x and circle
+const x_class = 'x'
+const circle_class = 'circle'
+//select all the cells
+const cellElements = document.querySelectorAll('[data-cell]')
+let circleTurn
 
-console.log($cellElems)
 
-const $cell = $('.cell')
+//loop through cells
+//add event listener that handle the click once its true. allows to fire the evenr once
+cellElements.forEach(Cell =>{
+    cell.addEventListener('click', handleClick, {once:true})
+})
 
+function handleClick(e) {
+    const cell = e.target
+    const currentClass = circleTurn ? circle_class : x_class
+    placemark(cell, currentClass)
+
+    //placeMark
+    //check for win
+    //check for Draw
+    //Switch turns
+}
